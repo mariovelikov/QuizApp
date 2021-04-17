38cb276befdf27a27ce2d34c7279433137e7b983
mariovelikov <70330129+mariovelikov@users.noreply.github.com>
2021-04-17 19:31:40 +0300

    remove 'Submit answer' button and 'Alert'

--- static/JavaScript/main.js
+++ static/JavaScript/main.js
@@ -1,19 +1,17 @@
-window.onload = initall()
+window.onload = initAll()
 var saveBtn;
 
 
-function initall() {
+function initAll() {
     saveBtn = document.getElementById('save_ans')
     saveBtn.onclick = saveAns
 }
 
 function saveAns() {
     var ans = $('input:radio[name=name]:checked').val()
-    alert('Answer is submitted !!!')
 
     var req = new XMLHttpRequest();
     var url = 'saveans?ans=' + ans
-    console.log(req)
 
     req.open('GET', url, true)
     req.send()
